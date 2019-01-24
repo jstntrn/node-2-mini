dea8dc8a03bd65e517802c21f12b9a76f3b3be85
jstntrn <justin.tran2290@gmail.com>
2019-01-24 12:34:01 -0700

    finished mini

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const ctrl = require('./controller')
 
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
-const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+massive(process.env.CONNECTION_STRING).then((dbInstance) => {
+    app.set('db', dbInstance)
+    // dbInstance.new_planes()
+    // .then((planes) => {console.log(planes)})
+    // .catch((err) => {console.log(err)})
+    
+    // dbInstance.get_planes()
+    // .then((planes) => {console.log(planes)})
+    // .catch((err) => console.log(err))
+});
+
+app.get(`/api/planes`, ctrl.getPlanes)
+
+
+const PORT = process.env.PORT || 3000
+app.listen( PORT , () => { console.log(`Server listening on port ${PORT}`); } );
 
